7fdd9ec04027a0150ad7c2590758d2d33697b1f2
Ernie Gurish <gurish.e@northeastern.edu>
2022-11-15 20:52:19 -0500

    added constructor and comments

--- models/User.ts
+++ models/User.ts
@@ -22,8 +22,31 @@ export default class User {
    private location: Location | null = null;
    private admin: boolean = false;
 
+   /**
+    * Creates a new User object.
+    * @param {string} id A unique ID for this object
+    * @param {string} username The text-based public name chosen by the User
+    * @param {string} password The User chosen account password
+    * @param {string} email The User's email
+    * @param {boolean} isAdmin Flag indicating if the user is an administrator
+    */
+   constructor(username: string, password: string, email: string = '', isAdmin: boolean = false) {
+      this.username = username;
+      this.password = password;
+      this.email = email;
+      this.admin = isAdmin;
+   }
+
+   /**
+    * Gets the admin status of the User
+    * @return {boolean} True if the user is an administrator, false otherwise
+    */
    get isAdmin() { return this.admin; }
-   
+
+   /**
+    * Sets the admin flag of the User to the given value
+    * @param {boolean} isAdminUser The flag indicating if this user should be an admin or not
+    */
    set isAdmin(isAdminUser: boolean) { this.admin = isAdminUser; }
 }
 
